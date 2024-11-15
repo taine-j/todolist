75267a407b7baaeeda9e16a5240eb055b0c67bee
Taine Jarvis <tainejarvis.dev@gmail.com>
2024-11-15 16:52:17 +1300

    chore: initialize project structure with core files and folders

---
+++ server/Controllers/TaskController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace todolist.Controllers;
+
+public class TaskController : Controller
+{
+    // 
+    // GET: /HelloWorld/
+    public string Index()
+    {
+        return "This is my default action...";
+    }
+    // 
+    // GET: /HelloWorld/Welcome/ 
+    public string Welcome()
+    {
+        return "This is the Welcome action method...";
+    }
+}
